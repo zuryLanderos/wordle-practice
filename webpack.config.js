9cf2c5ba2f44446bd66c2936932ccff0b950b74d
Zuriel Alejandro Landeros Mora <zurylanderos@gmail.com>
2023-04-22 22:27:05 -0600

    changes for deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-const path = require('path')
+const path = require("path");
+
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        path:path.resolve(__dirname,'public'),
-        filename: 'bundle.js'
-    },
-    mode: 'development'
-}
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  mode: "development",
+};
